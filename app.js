1bec02ed0801edb9a095208d0c7cdb30fedbbced
Alan Granea <aajj2215@gmail.com>
2025-02-10 18:22:59 -0300

    Terminando el código del JS

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let amigos = [];
+let lista = document.getElementById("listaAmigos");
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo").value.trim();
+
+    if (inputAmigo === "" || !/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(inputAmigo)) {
+        alert("¡Error! Ingrese un nombre válido (solo letras y espacios)");
+        document.getElementById("amigo").value = "";
+        return;
+    }
+
+    amigos.push(inputAmigo);
+    lista.innerHTML = amigos.map(amigo => `<li>${amigo}</li>`).join("");
+    document.getElementById("amigo").value = "";
+}
+
+function sortearAmigo(){
+    let resultado = document.getElementById('resultado');
+    let indice = Math.floor(Math.random() * amigos.length);
+
+    if(amigos.length === 0){
+        alert("Por favor. Ingrese un nombre");
+        return;
+    } else {
+        resultado.innerHTML = `Seleccionó a ${amigos[indice]} como su amigo/a secreto`;
+    }
+}
