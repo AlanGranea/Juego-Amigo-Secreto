62d23c97c463f47c8a693f6e8788bb1c22a1b0c5
Alan Granea <aajj2215@gmail.com>
2025-02-11 12:18:21 -0300

    Animaciones en el CSS y ajustes en el HTML y JS para mejorar la interacción

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
-
 let amigos = [];
 let lista = document.getElementById("listaAmigos");
 
 document.getElementById("amigo").addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
-        agregarAmigo()
+        event.preventDefault();  // Evita que el formulario se envíe o se realice algún comportamiento por defecto
+        agregarAmigo();
     }
 });
+
+function mostrarAlerta(mensaje) {
+    const alerta = document.getElementById("mensajeAlerta");
+    alerta.innerHTML = mensaje;
+    alerta.style.display = "block";
+
+    // Ocultar el mensaje después de 3 segundos
+    setTimeout(function() {
+        alerta.style.display = "none";
+    }, 3000);
+}
 
 function agregarAmigo() {
     let inputAmigo = document.getElementById("amigo").value.trim();
 
     if (inputAmigo === "" || !/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(inputAmigo)) {
-        alert("¡Error! Ingrese un nombre válido (solo letras y espacios)");
+        mostrarAlerta("¡Error! Ingrese un nombre válido (solo letras y espacios)");
         document.getElementById("amigo").value = "";
         return;
     }
 
     if(amigos.some(amigo => amigo.toUpperCase() === inputAmigo.toUpperCase())){
-        alert("Ese nombre está duplicado");
+        mostrarAlerta("Ese nombre está duplicado");
         document.getElementById("amigo").value = "";
         return;
     }
@@ -27,14 +38,17 @@ function agregarAmigo() {
     lista.innerHTML = amigos.map(amigo => `<li>${amigo}</li>`).join("");
     document.getElementById("amigo").value = "";
     document.getElementById("sorteoBtn").removeAttribute('disabled');
-
 }
+
+document.getElementById("sorteoBtn").removeAttribute('disabled');
 
 function sortearAmigo(){
     let resultado = document.getElementById('resultado');
     let indice = Math.floor(Math.random() * amigos.length);
 
-    if (amigos.length < 3){
+    if (amigos.length === 0){
+        resultado.innerHTML = "Por favor. Ingrese los nombres de sus amigos para el sorteo"
+    } else if (amigos.length < 3){
         resultado.innerHTML = "Por favor. Ingrese 3 o más amigos para el sorteo"
     } else {
         resultado.innerHTML = `Seleccionó a ${amigos[indice]} como su amigo/a secreto`;
@@ -47,9 +61,10 @@ function sortearAmigo(){
 
 function reiniciarSorteo() {
     amigos = [];
-    amigosSorteados = [];
     lista.innerHTML = '';
+    document.getElementById("sorteoBtn").removeAttribute('disabled');
     document.getElementById("amigo").removeAttribute("disabled");
     document.getElementById("agregarBtn").removeAttribute("disabled");
+    document.getElementById("reinicioBtn").setAttribute("disabled", "true");
     document.getElementById("resultado").innerHTML = '';
 }
